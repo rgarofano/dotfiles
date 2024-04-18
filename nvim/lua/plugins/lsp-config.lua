cc97e4c497b0c3809bd22affdd4d7b093d2108b0
Ryan Garofano <ryanjgarofano@gmail.com>
2024-04-17 20:56:21 -0700

    Add tailwind completions

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -36,6 +36,19 @@ return {
 					"--fallback-style=webkit",
 				},
 			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"html",
+					"css",
+					"scss",
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+					"vue",
+				},
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
