498ebe838d96f949296829910d9ee30e1b83de07
Ryan Garofano <ryanjgarofano@gmail.com>
2024-12-12 18:32:11 -0800

    Add keymap for cycling through LSP warnings and errors

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -6,46 +6,43 @@ vim.opt.tabstop = 8
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = false
-
--- Remap copy to system clipboard
-vim.keymap.set("n", "<leader>y", '"+y', {})
-vim.keymap.set("v", "<leader>y", '"+y', {})
-
--- Remove search highlighting
-vim.keymap.set("n", "<C-c>", "<cmd>nohlsearch<CR>")
-
 -- Line numbers
 vim.opt.relativenumber = true
-
 -- Line Wrapping
 vim.opt.breakindent = true
-
 -- Save undo history
 vim.opt.undofile = true
-
 -- Case-insensitive searching
 -- Except when one or more capital letters are in the search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
 -- Show markers for LSP errors
-vim.opt.signcolumn = 'yes'
-
+vim.opt.signcolumn = "yes"
 -- Decrease update time
 vim.opt.updatetime = 250
-
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
 vim.opt.timeoutlen = 300
-
 -- Minimum number of lines above and below the cursor
 vim.opt.scrolloff = 10
 
+-- Remap copy to system clipboard
+vim.keymap.set("n", "<leader>y", '"+y', {})
+vim.keymap.set("v", "<leader>y", '"+y', {})
+-- Remove search highlighting
+vim.keymap.set("n", "<C-c>", "<cmd>nohlsearch<CR>")
+-- Cycle through quick fix list
+vim.keymap.set("n", "<leader>cn", "<cmd>cnext<CR>", {})
+vim.keymap.set("n", "<leader>cp", "<cmd>cprevious<CR>", {})
+-- Cycle through LSP warnings/errors
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+
 -- Highlight when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function ()
-	vim.highlight.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
