8c1d300d7ed739da9e912ecfcbb5c43f612f08aa
Ryan Garofano <ryanjgarofano@gmail.com>
2025-01-22 22:33:21 -0800

    Add keybinds for git integrations and reformat config

--- nvim/lua/plugins/git-integration.lua
+++ nvim/lua/plugins/git-integration.lua
@@ -1,13 +1,15 @@
 return {
-	{
-		"tpope/vim-fugitive"
-	},
+    {
+        "tpope/vim-fugitive",
+    },
 
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-		end
-	}
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<CR>", {})
+            vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", {})
+        end,
+    },
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,42 +1,42 @@
 return {
-	"neovim/nvim-lspconfig",
-	lazy = false,
-	dependencies = {
-		{
-			"folke/lazydev.nvim",
-			ft = "lua", -- only load on lua files
-			opts = {
-				library = {
-					-- See the configuration section for more details
-					-- Load luvit types when the `vim.uv` word is found
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
-		},
-	},
-	config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-		})
-		lspconfig.clangd.setup({
-			capabilities = capabilities,
-			cmd = {
-				"clangd",
-				"--fallback-style=webkit",
-			},
-		})
-		lspconfig.svlangserver.setup({
-			cmd = { "svlangserver" },
-			filetypes = { "bluespec" },
-			root_dir = require("lspconfig").util.root_pattern(".git", "*.bsv"),
-		})
-		lspconfig.ts_ls.setup({
-			capabilities = capabilities,
-		})
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-	end,
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
+    },
+    config = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local lspconfig = require("lspconfig")
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+        })
+        lspconfig.clangd.setup({
+            capabilities = capabilities,
+            cmd = {
+                "clangd",
+                "--fallback-style=webkit",
+            },
+        })
+        lspconfig.svlangserver.setup({
+            cmd = { "svlangserver" },
+            filetypes = { "bluespec" },
+            root_dir = require("lspconfig").util.root_pattern(".git", "*.bsv"),
+        })
+        lspconfig.ts_ls.setup({
+            capabilities = capabilities,
+        })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
 }
