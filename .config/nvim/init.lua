ab97d2e449d0b6ba3f6f2e06ba1fe51e24aae109
Ryan Garofano <ryan@ryangarofano.ca>
2026-06-28 15:31:06 -0700

    Change to native vim.pack for plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,49 @@
--- Setup Plugin Manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("settings")
-require("lazy").setup("plugins")
+
+vim.pack.add({
+    "https://github.com/Mofiqul/vscode.nvim",           -- theme
+    "https://github.com/nvim-lua/plenary.nvim",         -- dependency
+    "https://github.com/nvim-tree/nvim-web-devicons",   -- dependency
+    "https://github.com/nvim-telescope/telescope.nvim", -- fancy find/grep
+    "https://github.com/windwp/nvim-autopairs",         -- autopairs
+    "https://github.com/lewis6991/gitsigns.nvim",       -- git integration
+    "https://github.com/neovim/nvim-lspconfig",         -- lsp
+    "https://github.com/saghen/blink.lib",              -- dependency
+    "https://github.com/saghen/blink.cmp",              -- completions
+})
+
+-- theme
+local theme = require("vscode")
+theme.setup({ style = "dark", transparent = true })
+theme.load()
+
+-- fancy find/grep
+require("telescope").setup({})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+vim.keymap.set("n", "gs", builtin.grep_string, {})
+vim.keymap.set("n", "<leader>fc", function()
+    builtin.find_files({
+        cwd = vim.fn.stdpath("config")
+    })
+end)
+
+-- autopairs
+require("nvim-autopairs").setup({})
+
+-- git integration
+require("gitsigns").setup()
+vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<Enter>")
+vim.keymap.set("n", "<leader>nh", "<cmd>Gitsigns next_hunk<Enter>")
+vim.keymap.set("n", "<leader>ph", "<cmd>Gitsigns prev_hunk<Enter>")
+vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame_line<Enter>")
+vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns reset_hunk<Enter>")
+
+-- lsp
+require("lsp")
+
+-- completions
+local cmp = require("blink.cmp")
+cmp.build():pwait()
+cmp.setup()
---
+++ .config/nvim/lua/lsp.lua
@@ -0,0 +1,44 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp.format", {}),
+    callback = function(args)
+        -- Format on save
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        if not client:supports_method("textDocument/willSaveWaitUntil")
+            and client:supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = vim.api.nvim_create_augroup("lsp.format", { clear = false }),
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                end,
+            })
+        end
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+        vim.diagnostic.config({ virtual_text = true })
+    end
+})
+
+vim.lsp.config["lua_ls"] = {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {
+                    "vim"
+                }
+            }
+        }
+    }
+}
+
+vim.lsp.config["clangd"] = {
+    cmd = {
+        "clangd",
+        "--clang-tidy",
+        "--fallback-style=webkit"
+    }
+}
+
+vim.lsp.enable({
+    "lua_ls",
+    "clangd"
+})
