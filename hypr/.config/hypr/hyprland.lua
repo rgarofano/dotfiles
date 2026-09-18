3e63fc2654befe5d3b254d3e2d5a5a0819347173
Ryan Garofano <ryanjgarofano@gmail.com>
2026-09-18 13:36:45 -0700

    User h,j,k,l to focus windows

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -281,13 +281,13 @@ hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({ action = "toggle" }))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + space", hl.dsp.exec_cmd(menu))
-hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit")) -- dwindle only
+hl.bind(mainMod .. " + X", hl.dsp.layout("togglesplit")) -- dwindle only
 
 -- Move focus with mainMod + arrow keys
-hl.bind(mainMod .. " + left", hl.dsp.focus({ direction = "left" }))
-hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
-hl.bind(mainMod .. " + up", hl.dsp.focus({ direction = "up" }))
-hl.bind(mainMod .. " + down", hl.dsp.focus({ direction = "down" }))
+hl.bind(mainMod .. " + H", hl.dsp.focus({ direction = "left" }))
+hl.bind(mainMod .. " + L", hl.dsp.focus({ direction = "right" }))
+hl.bind(mainMod .. " + K", hl.dsp.focus({ direction = "up" }))
+hl.bind(mainMod .. " + J", hl.dsp.focus({ direction = "down" }))
 
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
@@ -337,7 +337,7 @@ hl.bind(mainMod .. "+ P", hl.dsp.exec_cmd("quickshell ipc call powerPanel toggle
 hl.bind(mainMod .. "+ N", hl.dsp.exec_cmd(("quickshell ipc call notificationCenter toggle")))
 
 -- Lockscreen
-hl.bind(mainMod .. "+ L", hl.dsp.exec_cmd(("quickshell ipc call lock activate")))
+hl.bind(mainMod .. "+ SHIFT + L", hl.dsp.exec_cmd(("quickshell ipc call lock activate")))
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
